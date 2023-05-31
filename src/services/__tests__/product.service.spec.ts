39877517951858ce599f06fc65de75999e639e86
Avinash Lakhiani <alakhiani@yahoo.com>
2023-05-30 22:20:37 -0700

    test: Achieved 100% coverage for product.service.ts APIs (#3, #5, #7, #9)

--- src/services/__tests__/product.service.spec.ts
+++ src/services/__tests__/product.service.spec.ts
@@ -7,7 +7,7 @@ const chance = new Chance();
 
 
 
-describe('ProductService', () => {
+describe('ProductService::getProducts', () => {
 
     let mockProducts: Partial<IProduct>[] = [];
     let productService: ProductService;
@@ -63,5 +63,266 @@ describe('ProductService', () => {
 
         // Assert
         expect(console.log).toHaveBeenCalledWith(`➤ [service]: Got back ${mockProducts.length} products`);
+    });
+
+    test('+ve: getProducts should not log when LOG_LEVEL is "info"', async () => {
+        // Arrange
+        process.env.LOG_LEVEL = 'info';
+        console.log = jest.fn(); // Mock console.log
+
+        // Act
+        await productService.getProducts();
+
+        // Assert
+        expect(console.log).not.toHaveBeenCalled();
+    });
+});
+
+describe('ProductService::getProduct', () => {
+    let mockProductId: string;
+    let mockProduct: Partial<IProduct>;
+    let productService: ProductService;
+    let mockProductModel: Model<IProduct>;
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+
+        // Mock product data
+        mockProductId = chance.guid();
+        mockProduct = {
+            _id: mockProductId,
+            name: chance.word(),
+            description: chance.sentence(),
+            price: chance.floating({ min: 0, max: 100 }),
+            imageUrl: chance.url(),
+        };
+
+        // Mock the Product model
+        mockProductModel = {
+            findById: jest.fn().mockReturnThis(),
+            lean: jest.fn().mockReturnThis(),
+            exec: jest.fn().mockResolvedValue(mockProduct),
+        } as unknown as Model<IProduct>;
+
+        productService = new ProductService(mockProductModel);
+    });
+
+    test('+ve: should call findById and get back the product', async () => {
+        // Act
+        const result = await productService.getProduct(mockProductId);
+
+        // Assert
+        expect(mockProductModel.findById).toHaveBeenCalledWith(mockProductId);
+        expect(result).toEqual(mockProduct);
+    });
+
+    test('+ve: getProduct should log when LOG_LEVEL is "trace"', async () => {
+        // Arrange
+        process.env.LOG_LEVEL = 'trace';
+        console.log = jest.fn(); // Mock console.log
+
+        // Act
+        await productService.getProduct(mockProductId);
+
+        // Assert
+        expect(console.log).toHaveBeenCalledWith(`➤ [service]: Got back ${JSON.stringify(mockProduct)}`);
+    });
+
+    test('+ve: getProduct should not log when LOG_LEVEL is "info"', async () => {
+        // Arrange
+        process.env.LOG_LEVEL = 'info';
+        console.log = jest.fn(); // Mock console.log
+
+        // Act
+        await productService.getProduct(mockProductId);
+
+        // Assert
+        expect(console.log).not.toHaveBeenCalled();
+    });
+});
+
+describe('ProductService::createProduct', () => {
+    let productService: ProductService;
+    let mockProductId: string = chance.guid();
+    let mockProduct: Partial<IProduct>;
+    let mockCreatedProduct: Partial<IProduct>;
+    let mockProductModel: Model<IProduct>;
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+
+        // Mock product data
+        mockProduct = {
+            name: chance.word(),
+            description: chance.sentence(),
+            price: chance.floating({ min: 0, max: 100 }),
+            imageUrl: chance.url(),
+        };
+
+        mockCreatedProduct = { ...mockProduct, _id: mockProductId };
+
+        // Mock the Product model
+        mockProductModel = {
+            create: jest.fn().mockResolvedValue(mockCreatedProduct),
+        } as unknown as Model<IProduct>;
+
+        productService = new ProductService(mockProductModel);
+    });
+
+    test('+ve: should create a new product', async () => {
+        // Act
+        const result = await productService.createProduct(mockProduct as IProduct);
+
+        // Assert
+        expect(mockProductModel.create).toHaveBeenCalledWith(mockProduct);
+        expect(result).toEqual(mockCreatedProduct);
+    });
+
+    test('+ve: createProduct should log when LOG_LEVEL is "trace"', async () => {
+        // Arrange
+        process.env.LOG_LEVEL = 'trace';
+        console.log = jest.fn(); // Mock console.log
+
+        // Act
+        await productService.createProduct(mockProduct as IProduct);
+
+        // Assert
+        expect(console.log).toHaveBeenCalledWith(`➤ [service]: Created ${JSON.stringify(mockCreatedProduct)}`);
+    });
+
+    test('+ve: createProduct should not log when LOG_LEVEL is "info"', async () => {
+        // Arrange
+        process.env.LOG_LEVEL = 'info';
+        console.log = jest.fn(); // Mock console.log
+
+        // Act
+        await productService.createProduct(mockProduct as IProduct);
+
+        // Assert
+        expect(console.log).not.toHaveBeenCalled();
+    });
+});
+
+describe('ProductService::updateProduct', () => {
+    let productService: ProductService;
+    let mockProductId: string = chance.guid();
+    let mockProduct: Partial<IProduct>;
+    let mockUpdatedProduct: Partial<IProduct>;
+    let mockProductModel: Model<IProduct>;
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+
+        // Mock product data
+        mockProduct = {
+            name: chance.word(),
+            description: chance.sentence(),
+            price: chance.floating({ min: 0, max: 100 }),
+            imageUrl: chance.url(),
+        };
+
+        mockUpdatedProduct = { ...mockProduct, _id: mockProductId };
+
+        // Mock the Product model
+        mockProductModel = {
+            findByIdAndUpdate: jest.fn().mockReturnValue({ lean: jest.fn().mockReturnValue({ exec: jest.fn().mockResolvedValue(mockUpdatedProduct) }) }),
+        } as unknown as Model<IProduct>;
+
+        productService = new ProductService(mockProductModel);
+    });
+
+    test('+ve: should update the product', async () => {
+        // Act
+        const result = await productService.updateProduct(mockProductId, mockProduct as IProduct);
+
+        // Assert
+        expect(mockProductModel.findByIdAndUpdate).toHaveBeenCalledWith(mockProductId, mockProduct, { new: true, runValidators: true });
+        expect(result).toEqual(mockUpdatedProduct);
+    });
+
+    test('+ve: updateProduct should log when LOG_LEVEL is "trace"', async () => {
+        // Arrange
+        process.env.LOG_LEVEL = 'trace';
+        console.log = jest.fn(); // Mock console.log
+
+        // Act
+        await productService.updateProduct(mockProductId, mockProduct as IProduct);
+
+        // Assert
+        expect(console.log).toHaveBeenCalledWith(`➤ [service]: Updated ${JSON.stringify(mockUpdatedProduct)}`);
+    });
+
+    test('+ve: updateProduct should not log when LOG_LEVEL is "info"', async () => {
+        // Arrange
+        process.env.LOG_LEVEL = 'info';
+        console.log = jest.fn(); // Mock console.log
+
+        // Act
+        await productService.updateProduct(mockProductId, mockProduct as IProduct);
+
+        // Assert
+        expect(console.log).not.toHaveBeenCalled();
+    });
+});
+
+describe('ProductService::deleteProduct', () => {
+    let productService: ProductService;
+    let mockProductId: string = chance.guid();
+    let mockProduct: Partial<IProduct>;
+    let mockDeletedProduct: Partial<IProduct>;
+    let mockProductModel: Model<IProduct>;
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+
+        // Mock product data
+        mockProduct = {
+            name: chance.word(),
+            description: chance.sentence(),
+            price: chance.floating({ min: 0, max: 100 }),
+            imageUrl: chance.url(),
+        };
+
+        mockDeletedProduct = { ...mockProduct, _id: mockProductId };
+
+        // Mock the Product model
+        mockProductModel = {
+            findByIdAndDelete: jest.fn().mockReturnValue({ lean: jest.fn().mockReturnValue({ exec: jest.fn().mockResolvedValue(mockDeletedProduct) }) }),
+        } as unknown as Model<IProduct>;
+
+        productService = new ProductService(mockProductModel);
+    });
+
+    test('+ve: should delete the product', async () => {
+        // Act
+        const result = await productService.deleteProduct(mockProductId);
+
+        // Assert
+        expect(mockProductModel.findByIdAndDelete).toHaveBeenCalledWith(mockProductId);
+        expect(result).toEqual(mockDeletedProduct);
+    });
+
+    test('+ve: deleteProduct should log when LOG_LEVEL is "trace"', async () => {
+        // Arrange
+        process.env.LOG_LEVEL = 'trace';
+        console.log = jest.fn(); // Mock console.log
+
+        // Act
+        await productService.deleteProduct(mockProductId);
+
+        // Assert
+        expect(console.log).toHaveBeenCalledWith(`➤ [service]: Deleted ${JSON.stringify(mockDeletedProduct)}`);
+    });
+
+    test('+ve: deleteProduct should not log when LOG_LEVEL is "info"', async () => {
+        // Arrange
+        process.env.LOG_LEVEL = 'info';
+        console.log = jest.fn(); // Mock console.log
+
+        // Act
+        await productService.deleteProduct(mockProductId);
+
+        // Assert
+        expect(console.log).not.toHaveBeenCalled();
     });
 });
